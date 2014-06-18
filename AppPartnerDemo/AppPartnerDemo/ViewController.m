2237266b381177d66e6ae666fa9ee9fba72ec402
Anar Enhsaihan <aenhsaihan@gmail.com>
2014-06-17 22:12:43 -0400

    This fixes #2 using the following link: http://stackoverflow.com/questions/19082963/how-to-make-completely-transparent-navigation-bar-in-ios-7

--- AppPartnerDemo/AppPartnerDemo/ViewController.m
+++ AppPartnerDemo/AppPartnerDemo/ViewController.m
@@ -20,6 +20,11 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     NSLog(@"Loading");
+    
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+    self.navigationController.navigationBar.shadowImage = [UIImage new];
+    self.navigationController.navigationBar.translucent = YES;
+    self.navigationController.view.backgroundColor = [UIColor clearColor];
 }
 
 - (void)didReceiveMemoryWarning

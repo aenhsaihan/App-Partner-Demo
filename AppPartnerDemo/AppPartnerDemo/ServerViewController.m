1a2909ed76a7c6ad7c7ad13c2147d3acc1e73f39
Anar Enhsaihan <aenhsaihan@gmail.com>
2014-06-18 21:35:38 -0400

    Refactored the code for updating the labels.

--- AppPartnerDemo/AppPartnerDemo/ServerViewController.m
+++ AppPartnerDemo/AppPartnerDemo/ServerViewController.m
@@ -32,7 +32,11 @@
     
     self.title = @"SERVER PING";
     
+    [self.niceLabel setFont:[UIFont fontWithName:@"Machinato-Light" size:22.0]]; //FIXME: Reconfigure size for NICE!
+    [self.niceLabel setTextColor:[UIColor colorWithHexString:@"ff5f21"]];
     
+    [self.millisecondsLabel setFont:[UIFont fontWithName:@"Machinato-ExtraLightItalic" size:22]]; //FIXME: Reconfigure size for milliseconds
+    [self.millisecondsLabel setTextColor:[UIColor colorWithHexString:@"000000"]];
 }
 
 - (void)didReceiveMemoryWarning
@@ -76,12 +80,8 @@
 -(void)updateLabelsWithElapsedTime:(double)milliseconds
 {
     self.niceLabel.text = @"NICE!";
-    [self.niceLabel setFont:[UIFont fontWithName:@"Machinato-Light" size:22.0]]; //FIXME: Reconfigure size for NICE!
-    [self.niceLabel setTextColor:[UIColor colorWithHexString:@"ff5f21"]];
-    
+
     self.millisecondsLabel.text = [NSString stringWithFormat:@"%.2fms", milliseconds];
-    [self.millisecondsLabel setFont:[UIFont fontWithName:@"Machinato-ExtraLightItalic" size:22]]; //FIXME: Reconfigure size for milliseconds
-    [self.millisecondsLabel setTextColor:[UIColor colorWithHexString:@"000000"]];
 }
 
 @end
